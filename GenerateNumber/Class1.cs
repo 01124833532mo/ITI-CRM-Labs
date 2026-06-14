a4f8d5e1d1187e0a027632c4bcc9bc78eb524cfe
01124833532mo <mohammedhamdi726@gmail.com>
2026-06-14 15:59:04 +0300

    Add GenerateNumber project for CRM number sequence plugin
    A new project, `GenerateNumber`, has been added to the solution.
    This project implements a Dynamics CRM plugin to generate unique
    number sequences for entities on "Create" events.
    
    Key changes include:
    - Added `GenerateNumber` project entry to `DayOneCrm.sln`.
    - Implemented `Class1.cs` with `IPlugin` interface for sequence generation.
    - Created `GenerateNumber.csproj` with project properties and dependencies.
    - Added `AssemblyInfo.cs` with assembly metadata.
    - Added `app.config` for binding redirects.
    - Added `packages.config` with required NuGet dependencies.

---
+++ GenerateNumber/Class1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Linq;
+
+public class GenerateNumber : IPlugin
+{
+    public void Execute(IServiceProvider serviceProvider)
+    {
+        var context = (IPluginExecutionContext)
+            serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+        if (context.MessageName != "Create")
+            return;
+
+        var factory = (IOrganizationServiceFactory)
+            serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+        var service = factory.CreateOrganizationService(context.UserId);
+
+        var target = (Entity)context.InputParameters["Target"];
+
+        var query = new QueryExpression("ititrain_numbersequence")
+        {
+            ColumnSet = new ColumnSet(true)
+        };
+
+        query.Criteria.AddCondition(
+            "ititrain_tablename",
+            ConditionOperator.Equal,
+            context.PrimaryEntityName);
+
+        var sequence = service
+            .RetrieveMultiple(query)
+            .Entities
+            .FirstOrDefault();
+
+        if (sequence == null)
+            return;
+
+        string prefix = sequence.GetAttributeValue<string>("ititrain_sequence");
+        string columnName = sequence.GetAttributeValue<string>("ititrain_columnname");
+
+        int currentNumber =
+            sequence.GetAttributeValue<int>("ititrain_lastnumber");
+
+        currentNumber++;
+
+        target[columnName] = $"{prefix}{currentNumber:D5}";
+
+        sequence["ititrain_lastnumber"] = currentNumber;
+
+        service.Update(sequence);
+    }
+}
---
+++ GenerateNumber/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenerateNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenerateNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ec068a3-d80b-4d7d-8114-f817d0ccffbf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
